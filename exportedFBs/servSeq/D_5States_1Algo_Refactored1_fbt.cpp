78f1fdf97fbe76fe7e5503741769790df76b6d0f
zwerg4 <fab.gan@hotmail.de>
2024-02-19 10:57:28 +0100

    added refactored fbs for testing

---
+++ exportedFBs/servSeq/D_5States_1Algo_Refactored1_fbt.cpp
@@ -0,0 +1,202 @@
+/*************************************************************************
+ *** FORTE Library Element
+ ***
+ *** This file was generated using the 4DIAC FORTE Export Filter V1.0.x NG!
+ ***
+ *** Name: D_5States_1Algo_Refactored1
+ *** Description: Basic FB with empty ECC
+ *** Version:
+ ***     1.0: 2023-09-22/AK124910 -  -
+ *************************************************************************/
+
+#include "D_5States_1Algo_Refactored1_fbt.h"
+#ifdef FORTE_ENABLE_GENERATED_SOURCE_CPP
+#include "servSeq/D_5States_1Algo_Refactored1_fbt_gen.cpp"
+#endif
+
+#include "criticalregion.h"
+#include "resource.h"
+#include "forte_string.h"
+#include "forte_any_chars_variant.h"
+#include "forte_bool.h"
+#include "iec61131_functions.h"
+#include "forte_array_common.h"
+#include "forte_array.h"
+#include "forte_array_fixed.h"
+#include "forte_array_variable.h"
+
+DEFINE_FIRMWARE_FB(FORTE_servSeq__D_5States_1Algo_Refactored1, g_nStringIdservSeq__D_5States_1Algo_Refactored1)
+
+const CStringDictionary::TStringId FORTE_servSeq__D_5States_1Algo_Refactored1::scmDataOutputNames[] = {g_nStringIdOutputString};
+const CStringDictionary::TStringId FORTE_servSeq__D_5States_1Algo_Refactored1::scmDataOutputTypeIds[] = {g_nStringIdSTRING};
+const TForteInt16 FORTE_servSeq__D_5States_1Algo_Refactored1::scmEIWithIndexes[] = {-1, -1, -1, -1, -1};
+const CStringDictionary::TStringId FORTE_servSeq__D_5States_1Algo_Refactored1::scmEventInputNames[] = {g_nStringIdEI1, g_nStringIdEI2, g_nStringIdEI3, g_nStringIdEI4, g_nStringIdPrintHelloWorld};
+const TDataIOID FORTE_servSeq__D_5States_1Algo_Refactored1::scmEOWith[] = {0, scmWithListDelimiter};
+const TForteInt16 FORTE_servSeq__D_5States_1Algo_Refactored1::scmEOWithIndexes[] = {0};
+const CStringDictionary::TStringId FORTE_servSeq__D_5States_1Algo_Refactored1::scmEventOutputNames[] = {g_nStringIdAlgo1};
+const SFBInterfaceSpec FORTE_servSeq__D_5States_1Algo_Refactored1::scmFBInterfaceSpec = {
+  5, scmEventInputNames, nullptr, scmEIWithIndexes,
+  1, scmEventOutputNames, scmEOWith, scmEOWithIndexes,
+  0, nullptr, nullptr,
+  1, scmDataOutputNames, scmDataOutputTypeIds,
+  0, nullptr,
+  0, nullptr
+};
+
+FORTE_servSeq__D_5States_1Algo_Refactored1::FORTE_servSeq__D_5States_1Algo_Refactored1(const CStringDictionary::TStringId paInstanceNameId, forte::core::CFBContainer &paContainer) :
+    CBasicFB(paContainer, &scmFBInterfaceSpec, paInstanceNameId, nullptr),
+    var_conn_OutputString(var_OutputString),
+    conn_Algo1(this, 0),
+    conn_OutputString(this, 0, &var_conn_OutputString) {
+}
+
+void FORTE_servSeq__D_5States_1Algo_Refactored1::setInitialValues() {
+  var_OutputString = ""_STRING;
+}
+
+void FORTE_servSeq__D_5States_1Algo_Refactored1::executeEvent(TEventID paEIID, CEventChainExecutionThread *const paECET) {
+  do {
+    switch(mECCState) {
+      case scmStateSTART:
+        if(scmEventEI1ID == paEIID) enterStateState_1(paECET);
+        else
+        if(scmEventEI2ID == paEIID) enterStateState_2(paECET);
+        else
+        if(scmEventPrintHelloWorldID == paEIID) enterStateState_3(paECET);
+        else
+        if(scmEventEI4ID == paEIID) enterStateState_4(paECET);
+        else
+        if(scmEventEI3ID == paEIID) enterStateHW(paECET);
+        else return; //no transition cleared
+        break;
+      case scmStateState_1:
+        if(1) enterStateSTART(paECET);
+        else return; //no transition cleared
+        break;
+      case scmStateState_2:
+        if(1) enterStateSTART(paECET);
+        else return; //no transition cleared
+        break;
+      case scmStateState_3:
+        if(1) enterStateSTART(paECET);
+        else return; //no transition cleared
+        break;
+      case scmStateState_4:
+        if(1) enterStateSTART(paECET);
+        else return; //no transition cleared
+        break;
+      case scmStateHW:
+        if(1) enterStateSTART(paECET);
+        else return; //no transition cleared
+        break;
+      default:
+        DEVLOG_ERROR("The state is not in the valid range! The state value is: %d. The max value can be: 6.", mECCState.operator TForteUInt16 ());
+        mECCState = 0; // 0 is always the initial state
+        return;
+    }
+    paEIID = cgInvalidEventID; // we have to clear the event after the first check in order to ensure correct behavior
+  } while(true);
+}
+
+void FORTE_servSeq__D_5States_1Algo_Refactored1::enterStateSTART(CEventChainExecutionThread *const) {
+  mECCState = scmStateSTART;
+}
+
+void FORTE_servSeq__D_5States_1Algo_Refactored1::enterStateState_1(CEventChainExecutionThread *const) {
+  mECCState = scmStateState_1;
+}
+
+void FORTE_servSeq__D_5States_1Algo_Refactored1::enterStateState_2(CEventChainExecutionThread *const) {
+  mECCState = scmStateState_2;
+}
+
+void FORTE_servSeq__D_5States_1Algo_Refactored1::enterStateState_3(CEventChainExecutionThread *const) {
+  mECCState = scmStateState_3;
+}
+
+void FORTE_servSeq__D_5States_1Algo_Refactored1::enterStateState_4(CEventChainExecutionThread *const) {
+  mECCState = scmStateState_4;
+}
+
+void FORTE_servSeq__D_5States_1Algo_Refactored1::enterStateHW(CEventChainExecutionThread *const paECET) {
+  mECCState = scmStateHW;
+  alg_HWAlgo();
+  sendOutputEvent(scmEventAlgo1ID, paECET);
+}
+
+void FORTE_servSeq__D_5States_1Algo_Refactored1::readInputData(TEventID) {
+  // nothing to do
+}
+
+void FORTE_servSeq__D_5States_1Algo_Refactored1::writeOutputData(const TEventID paEIID) {
+  switch(paEIID) {
+    case scmEventAlgo1ID: {
+      writeData(0, var_OutputString, conn_OutputString);
+      break;
+    }
+    default:
+      break;
+  }
+}
+
+CIEC_ANY *FORTE_servSeq__D_5States_1Algo_Refactored1::getDI(size_t) {
+  return nullptr;
+}
+
+CIEC_ANY *FORTE_servSeq__D_5States_1Algo_Refactored1::getDO(const size_t paIndex) {
+  switch(paIndex) {
+    case 0: return &var_OutputString;
+  }
+  return nullptr;
+}
+
+CEventConnection *FORTE_servSeq__D_5States_1Algo_Refactored1::getEOConUnchecked(const TPortId paIndex) {
+  switch(paIndex) {
+    case 0: return &conn_Algo1;
+  }
+  return nullptr;
+}
+
+CDataConnection **FORTE_servSeq__D_5States_1Algo_Refactored1::getDIConUnchecked(TPortId) {
+  return nullptr;
+}
+
+CDataConnection *FORTE_servSeq__D_5States_1Algo_Refactored1::getDOConUnchecked(const TPortId paIndex) {
+  switch(paIndex) {
+    case 0: return &conn_OutputString;
+  }
+  return nullptr;
+}
+
+CIEC_ANY *FORTE_servSeq__D_5States_1Algo_Refactored1::getVarInternal(size_t) {
+  return nullptr;
+}
+
+void FORTE_servSeq__D_5States_1Algo_Refactored1::alg_HWAlgo(void) {
+
+  #line 2 "D_5States_1Algo_Refactored1.fbt"
+  var_OutputString = func_CONCAT(var_OutputString, "H"_STRING);
+  #line 3 "D_5States_1Algo_Refactored1.fbt"
+  var_OutputString = func_CONCAT(var_OutputString, "E"_STRING);
+  #line 4 "D_5States_1Algo_Refactored1.fbt"
+  var_OutputString = func_CONCAT(var_OutputString, "L"_STRING);
+  #line 5 "D_5States_1Algo_Refactored1.fbt"
+  var_OutputString = func_CONCAT(var_OutputString, "L"_STRING);
+  #line 6 "D_5States_1Algo_Refactored1.fbt"
+  var_OutputString = func_CONCAT(var_OutputString, "O"_STRING);
+  #line 7 "D_5States_1Algo_Refactored1.fbt"
+  var_OutputString = func_CONCAT(var_OutputString, " "_STRING);
+  #line 8 "D_5States_1Algo_Refactored1.fbt"
+  var_OutputString = func_CONCAT(var_OutputString, "W"_STRING);
+  #line 9 "D_5States_1Algo_Refactored1.fbt"
+  var_OutputString = func_CONCAT(var_OutputString, "O"_STRING);
+  #line 10 "D_5States_1Algo_Refactored1.fbt"
+  var_OutputString = func_CONCAT(var_OutputString, "R"_STRING);
+  #line 11 "D_5States_1Algo_Refactored1.fbt"
+  var_OutputString = func_CONCAT(var_OutputString, "L"_STRING);
+  #line 12 "D_5States_1Algo_Refactored1.fbt"
+  var_OutputString = func_CONCAT(var_OutputString, "D"_STRING);
+  #line 13 "D_5States_1Algo_Refactored1.fbt"
+  var_OutputString = func_CONCAT(var_OutputString, "!"_STRING);
+}
+
---
+++ exportedFBs/servSeq/D_5States_1Algo_Refactored2_fbt.h
@@ -0,0 +1,114 @@
+/*************************************************************************
+ *** FORTE Library Element
+ ***
+ *** This file was generated using the 4DIAC FORTE Export Filter V1.0.x NG!
+ ***
+ *** Name: D_5States_1Algo_Refactored2
+ *** Description: Basic FB with empty ECC
+ *** Version:
+ ***     1.0: 2023-09-22/AK124910 -  -
+ *************************************************************************/
+
+#pragma once
+
+#include "basicfb.h"
+#include "forte_string.h"
+#include "iec61131_functions.h"
+#include "forte_array_common.h"
+#include "forte_array.h"
+#include "forte_array_fixed.h"
+#include "forte_array_variable.h"
+
+
+class FORTE_servSeq__D_5States_1Algo_Refactored2 final : public CBasicFB {
+  DECLARE_FIRMWARE_FB(FORTE_servSeq__D_5States_1Algo_Refactored2)
+
+  private:
+    static const CStringDictionary::TStringId scmDataOutputNames[];
+    static const CStringDictionary::TStringId scmDataOutputTypeIds[];
+    static const TEventID scmEventEI1ID = 0;
+    static const TEventID scmEventEI2ID = 1;
+    static const TEventID scmEventEI3ID = 2;
+    static const TEventID scmEventEI4ID = 3;
+    static const TEventID scmEventPrintHelloWorldID = 4;
+    static const TForteInt16 scmEIWithIndexes[];
+    static const CStringDictionary::TStringId scmEventInputNames[];
+    static const TEventID scmEventAlgo1ID = 0;
+    static const TDataIOID scmEOWith[];
+    static const TForteInt16 scmEOWithIndexes[];
+    static const CStringDictionary::TStringId scmEventOutputNames[];
+
+    static const SFBInterfaceSpec scmFBInterfaceSpec;
+
+    CIEC_ANY *getVarInternal(size_t) override;
+
+    void alg_HWAlgo(void);
+
+    static const TForteInt16 scmStateSTART = 0;
+    static const TForteInt16 scmStateState_1 = 1;
+    static const TForteInt16 scmStateState_2 = 2;
+    static const TForteInt16 scmStateState_3 = 3;
+    static const TForteInt16 scmStateState_4 = 4;
+    static const TForteInt16 scmStateHW = 5;
+
+    void enterStateSTART(CEventChainExecutionThread *const paECET);
+    void enterStateState_1(CEventChainExecutionThread *const paECET);
+    void enterStateState_2(CEventChainExecutionThread *const paECET);
+    void enterStateState_3(CEventChainExecutionThread *const paECET);
+    void enterStateState_4(CEventChainExecutionThread *const paECET);
+    void enterStateHW(CEventChainExecutionThread *const paECET);
+
+    void executeEvent(TEventID paEIID, CEventChainExecutionThread *const paECET) override;
+
+    void readInputData(TEventID paEIID) override;
+    void writeOutputData(TEventID paEIID) override;
+    void setInitialValues() override;
+
+  public:
+    FORTE_servSeq__D_5States_1Algo_Refactored2(CStringDictionary::TStringId paInstanceNameId, forte::core::CFBContainer &paContainer);
+
+    CIEC_STRING var_OutputString;
+
+    CIEC_STRING var_conn_OutputString;
+
+    CEventConnection conn_Algo1;
+
+    CDataConnection conn_OutputString;
+
+    CIEC_ANY *getDI(size_t) override;
+    CIEC_ANY *getDO(size_t) override;
+    CEventConnection *getEOConUnchecked(TPortId) override;
+    CDataConnection **getDIConUnchecked(TPortId) override;
+    CDataConnection *getDOConUnchecked(TPortId) override;
+
+    void evt_EI1(CIEC_STRING &paOutputString) {
+      executeEvent(scmEventEI1ID, nullptr);
+      paOutputString = var_OutputString;
+    }
+
+    void evt_EI2(CIEC_STRING &paOutputString) {
+      executeEvent(scmEventEI2ID, nullptr);
+      paOutputString = var_OutputString;
+    }
+
+    void evt_EI3(CIEC_STRING &paOutputString) {
+      executeEvent(scmEventEI3ID, nullptr);
+      paOutputString = var_OutputString;
+    }
+
+    void evt_EI4(CIEC_STRING &paOutputString) {
+      executeEvent(scmEventEI4ID, nullptr);
+      paOutputString = var_OutputString;
+    }
+
+    void evt_PrintHelloWorld(CIEC_STRING &paOutputString) {
+      executeEvent(scmEventPrintHelloWorldID, nullptr);
+      paOutputString = var_OutputString;
+    }
+
+    void operator()(CIEC_STRING &paOutputString) {
+      evt_EI1(paOutputString);
+    }
+};
+
+
